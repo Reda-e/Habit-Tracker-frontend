2a0aec153c1441f5697f319a4a784b9d103883fd
Reda <redar9596@gmail.com>
2026-03-18 22:31:32 +0000

    Modification de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,23 @@ import ProtectedRoute from "./ProtectedRoute";
 import Login from "./pages/Login";
 import Habits from "./pages/Habits";
 import HabitLogs from "./pages/HabitLogs";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
 import Notifications from "./pages/Notifications";
+import Register from "./pages/Register";
 import Header from "./components/Header";
 
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Header />
+        {/* <Header /> */}
         <div className="p-4">
           <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
             <Route path="/" element={
               <ProtectedRoute><Habits /></ProtectedRoute>
             } />
