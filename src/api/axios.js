2fac7d5a8fead4d30959aa4e9042e1398ee8b26a
Reda <redar9596@gmail.com>
2026-03-18 22:31:41 +0000

    Modification de axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:8000/api", // غير حسب Laravel API ديالك
-  headers: { "Content-Type": "application/json" },
-   // Permet l'envoi automatique des cookies
+  headers: { Accept: "application/json" },
+  // Permet l'envoi automatique des cookies
   withCredentials: true,
   // Ajout automatiquement le token CSRF dans les requetes protegees
   withXSRFToken: true,
